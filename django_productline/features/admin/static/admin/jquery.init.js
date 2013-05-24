465902d540d983595243e34d3f4eb794c7e10d19
Toni Michel <toni@Ranjid-V.fritz.box>
2013-05-24 23:45:05 +0200

    overwrote django jquery files avoid django-jquery vs. jsbasics-jquery conflicts

---
+++ django_productline/features/admin/static/admin/jquery.init.js
@@ -0,0 +1,3 @@
+var django = {
+    "jQuery": $
+}
---
+++ django_productline/features/admin/static/admin/jquery.js
@@ -0,0 +1,5 @@
+ /*
+    Overload the default django-shipped, out-dated jquery.js to avoid
+    conflicts with the new one brought by jsbasics.
+    Must not be included in media_includes as it is included by django itself.
+ */
